e3a229e7ccec6b945a3d6b8638898ea177469a01
Arun Nair <appuarunnair@gmail.com>
2016-10-17 10:27:50 +0530

    object found page

---
+++ objectFound.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Lost & Found</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <style>
+
+            .jumbo-trans{
+                background: transparent;
+                margin: auto;
+            }
+
+            body{
+                background-image: url("images/alternative_bg_img.jpg");
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <button type="button" class="btn btn-primary" id="logout" style="margin-left: 90%; margin-top:15px;">Logout</button>
+            <div class="jumbotron jumbo-trans">
+                <h1 class="text-center">YOUR LOST ITEM HAS BEEN FOUND!</h1>
+                <h1 class="text-center">BY</h1>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Person's username<br><br>Person's Mobile No.<br><br>Person's Email ID</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <center><img src="images/vault_boy_thubs_up.png" width="200px" height="200px" style="background-size: cover"></center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
